d327bf9001ea7e4e94772ae1a7d4d44edf01e7fe
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-11-15 15:43:47 -0700

    Feature Engineering and Random Forest

--- ItemDemand/ItemDemand.R
+++ ItemDemand/ItemDemand.R
@@ -44,3 +44,55 @@ x4 <- storeItemTrain10_47 %>%
 
 gridExtra::grid.arrange(x1,x2,x3,x4)
   
+###########################################
+storeItem <- itemtrain %>%
+  filter(store == 4, item == 14)
+storeItem
+
+
+myrecipe <- recipe(sales ~ ., data = storeItem) %>%
+  step_date(date, features="dow") %>%
+  step_date(date, features="month") %>%
+  #step_date(date, features ="decimal") %>%
+  step_date(date, features = "year") %>%
+  step_rm(store, item) %>%
+  step_date(date, features="doy") %>%
+  step_range(date_doy, min = 0, max=pi) %>%
+  step_mutate(sinDOY = sin(date_doy), cosDOY=cos(date_doy)) %>%
+  step_holiday(date, holidays = "ChristmasDay")
+
+prepped_recipe <- prep(myrecipe)      
+baked_recipe <- bake(prepped_recipe, new_data = itemtrain)
+
+
+#################################################################################
+# Random Forests
+#################################################################################
+#install.packages("ranger")
+library(tidymodels)
+my_mod <- rand_forest(mtry = tune(),
+                      min_n=tune(),
+                      trees=500) %>% #Type of model
+  set_engine("ranger") %>% # What R function to use
+  set_mode("regression") 
+## Create a workflow with model & recipe
+randfor_wf <- workflow() %>%
+  add_recipe(myrecipe) %>%
+  add_model(my_mod)
+## Set up grid of tuning values
+tuning_grid <- grid_regular(mtry(range = c(1,(ncol(storeItem)-1))), min_n(), levels = 5)
+## Set up K-fold CV
+folds <- vfold_cv(storeItem, v = 5, repeats = 1)
+## Find best tuning parameters
+CV_results <- randfor_wf %>%
+  tune_grid(resamples = folds, grid = tuning_grid, metrics=metric_set(smape))
+
+
+bestTune <- CV_results %>%
+  select_best("smape")
+
+collect_metrics(CV_results) %>%
+  filter(mtry==bestTune$mtry, min_n == bestTune$min_n) %>%
+  pull(mean)
+  
+
